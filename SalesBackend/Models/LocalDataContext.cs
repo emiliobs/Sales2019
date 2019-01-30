262eab96c414b918f811556e178565d9e39edd68
Emilio Barrera <barrera_emilio@hotmail.com>
2019-01-30 00:09:14 +0000

    service getlist

---
+++ SalesBackend/Models/LocalDataContext.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesBackend.Models
+{
+    using SalesDomain;
+
+    public class LocalDataContext  : DataContext
+    {
+        public System.Data.Entity.DbSet<SalesCommon.Product> Products { get; set; }
+    }
+}
---
+++ SalesDomain/DataContext.cs
@@ -0,0 +1,19 @@
+﻿namespace SalesDomain
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class DataContext : DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<SalesCommon.Product> Products { get; set; }
+    }
+}
