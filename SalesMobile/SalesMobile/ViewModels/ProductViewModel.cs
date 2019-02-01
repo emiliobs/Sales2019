c001ff4762196b5f331023e6d6562ffdb9267ccd
Emilio Barrera <barrera_emilio@hotmail.com>
2019-02-01 23:01:26 +0000

    refresh listview

--- SalesMobile/SalesMobile/ViewModels/ProductViewModel.cs
+++ SalesMobile/SalesMobile/ViewModels/ProductViewModel.cs
@@ -1,11 +1,13 @@
 ﻿namespace SalesMobile.ViewModels
 {
+    using GalaSoft.MvvmLight.Command;
     using SalesCommon;
     using SalesMobile.Services;
     using System;
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
     using System.Text;
+    using System.Windows.Input;
     using Xamarin.Forms;
 
     public class ProductViewModel : BaseViewModel
@@ -17,10 +19,26 @@
         #region Attributes
 
         private ObservableCollection<Product> products;
+        private bool isRefreshing;
+        
 
         #endregion
 
         #region Properties
+
+        public bool IsRefreshing
+        {
+            get => isRefreshing;
+            set
+            {
+                if (isRefreshing != value)
+                {
+                    isRefreshing = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
         public ObservableCollection<Product> Products
         {
             get => products;
@@ -45,15 +63,31 @@
 
         #endregion
 
+        #region Comands
+
+
+        public ICommand RefreshCommand => new RelayCommand(LoadProducts);
+        
+        #endregion
+
         #region Methods
+
         private async void LoadProducts()
         {
+            this.IsRefreshing = true;
+
             var response = await this.apiService.GetList<Product>("https://salesapiservice.azurewebsites.net", "/api", "/Products");
+            //var urlProducts = Application.Current.Resources["UrlApiProducts"].ToString();
+            //var prefix = Application.Current.Resources["Prefix"].ToString();
+            //var ProductsController = Application.Current.Resources["ProductsController"].ToString();
+            //var response = await this.apiService.GetList<Product>(urlProducts,prefix,ProductsController);
 
             //Aqui pregunto si llego algo de la api
             if (!response.IsSuccess)
             {
                 await Application.Current.MainPage.DisplayAlert("Error,",response.Message,"Accept.");
+
+                this.IsRefreshing = false;
 
                 return;
             }
@@ -63,6 +97,8 @@
 
             //aqui convierto la lista a una lista de obsevableCollection:
             this.Products = new ObservableCollection<Product>(list);
+
+            this.IsRefreshing = false;
 
 
         }
