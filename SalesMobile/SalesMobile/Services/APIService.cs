72ff6a8bfbfd6e3abb1edbb775a5c731c51ea61b
emilio barrera <barrera_emilio@hotmail.com>
2019-02-21 16:44:59 +0000

    AddProductViewModel one

--- SalesMobile/SalesMobile/Services/APIService.cs
+++ SalesMobile/SalesMobile/Services/APIService.cs
@@ -7,6 +7,7 @@
     using System;
     using System.Collections.Generic;
     using System.Net.Http;
+    using System.Text;
     using System.Threading.Tasks;
 
     public class APIService
@@ -24,7 +25,7 @@
 
             }
 
-            var isReachable = await CrossConnectivity.Current.IsRemoteReachable(Languages.google_com);
+            bool isReachable = await CrossConnectivity.Current.IsRemoteReachable(Languages.google_com);
             if (!isReachable)
             {
                 return new Response
@@ -44,11 +45,13 @@
         {
             try
             {
-                var client = new HttpClient();
-                client.BaseAddress = new Uri(urlBase);
-                var url = $"{prefix}{controller}";
-                var response = await client.GetAsync(url);
-                var result = await response.Content.ReadAsStringAsync();
+                HttpClient client = new HttpClient
+                {
+                    BaseAddress = new Uri(urlBase)
+                };
+                string url = $"{prefix}{controller}";
+                HttpResponseMessage response = await client.GetAsync(url);
+                string result = await response.Content.ReadAsStringAsync();
                 if (!response.IsSuccessStatusCode)
                 {
                     return new Response()
@@ -59,12 +62,55 @@
                     };
                 }
 
-                var list = JsonConvert.DeserializeObject<List<T>>(result);
+                List<T> list = JsonConvert.DeserializeObject<List<T>>(result);
 
-                return  new Response()
+                return new Response()
                 {
                     IsSuccess = true,
                     Result = list,
+                };
+
+            }
+            catch (Exception ex)
+            {
+
+                return new Response()
+                {
+                    IsSuccess = false,
+                    Message = ex.Message,
+                };
+            }
+
+        }
+        public async Task<Response> Post<T>(string urlBase, string prefix, string controller, T model)
+        {
+            try
+            {
+                string request = JsonConvert.SerializeObject(model);
+                StringContent content = new StringContent(request, Encoding.UTF8, "application/json");
+                HttpClient client = new HttpClient
+                {
+                    BaseAddress = new Uri(urlBase)
+                };
+                string url = $"{prefix}{controller}";
+                HttpResponseMessage response = await client.PostAsync(url, content);
+                string result = await response.Content.ReadAsStringAsync();
+                if (!response.IsSuccessStatusCode)
+                {
+                    return new Response()
+                    {
+                        IsSuccess = true,
+                        Message = result,
+
+                    };
+                }
+
+                T obj = JsonConvert.DeserializeObject<T>(result);
+
+                return new Response()
+                {
+                    IsSuccess = true,
+                    Result = obj,
                 };
 
             }
--- SalesMobile/SalesMobile/ViewModels/AddProductViewModel.cs
+++ SalesMobile/SalesMobile/ViewModels/AddProductViewModel.cs
@@ -1,15 +1,20 @@
 ﻿namespace SalesMobile.ViewModels
 {
     using GalaSoft.MvvmLight.Command;
+    using SalesCommon;
     using SalesMobile.Helpers;
-    using System;
-    using System.Collections.Generic;
-    using System.Text;
+    using SalesMobile.Services;
     using System.Windows.Input;
     using Xamarin.Forms;
 
-    public class AddProductViewModel :BaseViewModel
+    public class AddProductViewModel : BaseViewModel
     {
+        #region Services
+
+        private APIService aPIService;
+
+        #endregion
+
         #region Attributes
         private bool isRunning;
         private bool isEnabled;
@@ -51,6 +56,9 @@
         #region Contructors
         public AddProductViewModel()
         {
+            //Services
+            aPIService = new APIService();
+
             IsEnabled = true;
         }
         #endregion
@@ -76,7 +84,7 @@
                 return;
             }
 
-            var price = decimal.Parse(Price);
+            decimal price = decimal.Parse(Price);
 
             if (price < 0)
             {
@@ -84,7 +92,55 @@
                 return;
             }
 
-            await Application.Current.MainPage.DisplayAlert(Languages.Error, "Todo va bien......", Languages.Accept);
+
+            IsRunning = true;
+            IsEnabled = false;
+
+            //check intternet:
+            var connection = await aPIService.CheckConnection();
+            if (!connection.IsSuccess)
+            {
+                IsRunning = false;
+                IsEnabled = true;
+
+                await Application.Current.MainPage.DisplayAlert(Languages.Error, connection.Message, Languages.Accept);
+
+                return;
+            }
+
+            //aqui capturo loas datos que el usurio ingreso por el formulario:
+            var product = new Product()
+            {
+              Description = Description,
+              Price = price,
+              Remarks = Remarks,
+              
+            };
+
+            //aqyui consumo el API del post:
+            string urlProducts = Application.Current.Resources["UrlApiProducts"].ToString();
+            string prefix = Application.Current.Resources["Prefix"].ToString();
+            string ProductsController = Application.Current.Resources["ProductsController"].ToString();
+            var response = await this.aPIService.Post(urlProducts, prefix, ProductsController, product);
+
+            if (!response.IsSuccess)
+            {
+
+                IsRunning = false;
+                IsEnabled = true;
+
+                await Application.Current.MainPage.DisplayAlert(Languages.Error, response.Message, Languages.Accept);   
+                return;
+
+            }
+
+            
+
+            IsRunning = false;
+            IsEnabled = true;
+            
+            //Aqui regreso a la pagina anterior
+            await Application.Current.MainPage.Navigation.PopAsync();
 
 
         }
